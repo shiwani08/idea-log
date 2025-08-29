8c98743d9a070ecd128372473e9a597c36fa5e95
shiwani08 <shiwanisoni29082002@gmail.com>
2025-08-29 18:18:58 +0530

    Middleware resource - added

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,6 @@
 // middleware.ts
+
+// https://youtu.be/Fx8c3dQD_TI?si=ykuBI14UzTCftoPB - use this for adding the middleware value
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
