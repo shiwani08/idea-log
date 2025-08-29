3d3e0d20306c6d0944e8257c86a636fdb7688d4d
shiwani08 <shiwanisoni29082002@gmail.com>
2025-08-29 16:52:42 +0530

    Middlewares - in progress

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,5 @@
 // middleware.ts
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
-  // your middleware logic
-  return NextResponse.next();
-}
+export { auth as middleware } from "@/auth";
